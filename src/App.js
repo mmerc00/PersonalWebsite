71b54fe2462f12593e4aad38b2ed895287ea3648
Melissa Mercado <melissa.a.merc@gmail.com>
2021-01-28 14:45:49 -0500

    working on hamburger

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+// import Hamburger from "./components/Hamburger";
 
 function App() {
   return (
     <Router>
-      <Navbar />{" "}
+      {/* <Hamburger /> */}
+      <Navbar />
     </Router>
   );
 }
---
+++ src/components/Hamburger/index.js
@@ -0,0 +1,15 @@
+/* import React from "react";
+import { HamburgerContainer, Icon, CloseIcon } from "./HamburgerElements";
+
+const Hamburger = () => {
+  return (
+    <HamburgerContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+    </HamburgerContainer>
+  );
+};
+
+export default Hamburger;
+ */
