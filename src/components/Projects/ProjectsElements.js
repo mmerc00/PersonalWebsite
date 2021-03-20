91c24bac63aeef2c8e4096b17067c4c3e917280d
Melissa Mercado <melissa.a.merc@gmail.com>
2021-03-20 19:16:12 -0400

    changed up max-width for project cards

--- src/components/Projects/ProjectsElements.js
+++ src/components/Projects/ProjectsElements.js
@@ -14,7 +14,7 @@ export const ProjectsContainer = styled.div`
   }
 `;
 export const ProjectsWrap = styled.div`
-  max-width: 1000px;
+  max-width: 1660px;
   margin: 0 auto;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
