56d20beb299ce86916460eec6b62b6350c7a8fe1
Melissa Mercado <melissa.a.merc@gmail.com>
2021-04-30 19:17:02 -0400

    added icons to connect section

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -7,13 +7,19 @@ import {
   ConnectWrap,
   Book,
 } from "./ConnectElements";
-//insert wrapper
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { GrMailOption } from "react-icons/gr";
+
 const Connect = () => {
   return (
     <ConnectContainer id="connect">
       <Book src={book} alt="divider" />
       <ConnectWrap>
         <ConnectH1>Let's Connect</ConnectH1>
+        <FaLinkedin size="3em" color="433e0e" />
+        <GrMailOption size="3em" color="433e0e" />
+
+        <FaGithub size="3em" color="433e0e" />
         <ConnectP>Open for collaborations and full-stack positions</ConnectP>
       </ConnectWrap>
     </ConnectContainer>
